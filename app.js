8dc3798058892efc5c53074bd7cd27b286c8b11d
johnhorsema <jjmace01@gmail.com>
2017-08-20 13:38:47 +0800

    second

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ angular.module('myApp', [
       reader.readAsDataURL(e.target.files[0]);
   }
 })
-.controller('TagCtrl', function($http){
+.controller('TagCtrl', function($scope, $http){
   var accessToken = "a0e3d49509684e11a150e8afff2022e8";
   var baseUrl = "https://api.api.ai/v1/";
   this.response_list = [];
@@ -110,6 +110,123 @@ angular.module('myApp', [
       // or server returns response with an error status.
     });
   }
+
+this.handleImage = function(e) {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        $("#file_logo").hide();
+        $('#uploaded').show();
+        $('#uploaded').attr('src',event.target.result);
+        self.img_data = event.target.result;
+                console.log('fired');
+        var passable = self.makeblob(self.img_data);
+        img_post(passable);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+
+    // img_post(self.makeblob(data));
+}
+
+// function previewFile(){
+//     // var preview = document.getElementById("uploaded");
+//     var file = document.querySelector('input[type=file]').files[0];
+//     var reader = new FileReader();
+
+
+// }
+
+
+this.makeblob = function(dataURL){
+    var base64_maker = ';base64,';
+    var parts = dataURL.split(base64_maker);
+    var contentType = parts[0].split(':')[1];
+    var raw  = window.atob(parts[1]);
+    var rawLength = raw.length;
+    var uint8arr = new Uint8Array(rawLength);
+    for (var i=0; i<rawLength; i++){
+      uint8arr[i] = raw.charCodeAt(i);
+    }
+    return new Blob([uint8arr], {type:contentType});
+  }
+function json_parser(descriptions){
+    // text = document.getElementById('jsondisplay');
+    var display_json = feat_extract(descriptions);
+    return display_json;
+        // console.log(display_json);
+        // self.azure = display_json;
+    // text.value = JSON.stringify(display_json, null, 4);
+  }
+
+function feat_extract(jsonformat){
+  //This is specific to ms azure
+  var tags = jsonformat['description']['tags'];
+  var verbal_description = jsonformat['description']['captions'];
+  var colors = jsonformat['color']['dominantColors'];
+
+
+    var ind = tags.indexOf('indoor')
+    var outd = tags.indexOf('outdoor')
+    if (ind>0){
+        // console.log('indoor'.indexOf(tag))
+        tags.splice(ind,1)
+    }
+    else if (outd){
+        tags.splice(outd,1)
+    }
+
+  var proc_json = {
+    'tags':tags.splice(0,3),
+    'description': verbal_description,
+    'dominant colors':colors
+  }
+  return proc_json
+}
+function img_post(raw_data) {
+    self.azured = true;
+    var params = {
+        // Request parameters
+        "visualFeatures":'Categories, Tags, Description, Color',
+        "language":'en'
+    };
+
+    $http({
+      method: 'POST',
+      url: "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + $.param(params),
+      headers: {
+         'Content-Type': 'application/octet-stream',
+         "Ocp-Apim-Subscription-Key": "a947bbe94cb342d3baecae9040362f4d"
+       },
+      data: raw_data
+    }).then(function successCallback(response) {
+      self.azure = json_parser(response.data);
+      console.log(self.azure);
+    });
+  
+    // $.ajax({
+    //     url: "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + $.param(params),
+    //     beforeSend: function(xhrObj){
+    //         // Request headers
+    //         xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+    //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","a947bbe94cb342d3baecae9040362f4d");
+    //     },
+    //     type: "POST",
+    //     // Request body
+    //     data: raw_data,
+    //     processData:false
+    // })
+    // .done(function(data) {
+    //   self.azure = json_parser(data);
+    // })
+};
+})
+.directive('customOnChange', function() {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var onChangeHandler = scope.$eval(attrs.customOnChange);
+      element.bind('change', onChangeHandler);
+    }
+  };
 })
 .run(['$rootScope','$window', function($rootScope, $window) {
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
